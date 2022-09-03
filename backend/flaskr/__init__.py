d8a1a9001be2ede30e06b126e78bbc9045e28a13
Iandavidk <eandavid6@gmail.com>
2022-09-03 12:26:20 +0000

    Added questions and categories endpoints

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -34,6 +34,19 @@ def create_app(test_config=None):
     Create an endpoint to handle GET requests
     for all available categories.
     """
+    @app.route('/categories')
+    def retrieve_categories():
+        categories = Category.query.order_by(Category.id).all()
+        all_categories = [category.format() for category in categories]
+    
+    if len(all_categories) == 0:
+      abort(404)
+
+    return jsonify({
+      'success': True,
+      'categories': all_categories,
+      'total_categories': len(all_categories)
+    })
 
 
     """
@@ -48,6 +61,33 @@ def create_app(test_config=None):
     ten questions per page and pagination at the bottom of the screen for three pages.
     Clicking on the page numbers should update the questions.
     """
+    def paginate_questions(request, selection):
+        page = request.args.get('page', 1, type=int)
+        start =  (page - 1) * QUESTIONS_PER_PAGE
+        end = start + QUESTIONS_PER_PAGE
+
+        questions = [question.format() for question in selection]
+        current_questions = questions[start:end]
+
+        return current_questions
+
+    @app.route('/questions')
+    def retrieve_questions():
+        questions = Question.query.order_by(Question.id).all()
+        current_questions = paginate_questions(request, questions)
+        categories = Category.query.all()
+
+        if len(current_questions) == 0:
+            abort(404)
+
+        return jsonify({
+            'success': True,
+            'questions': current_questions,
+            'total_questions': len(questions),
+            "categories": [category.format() for category in categories],
+            'current_category': None
+        })
+
 
     """
     @TODO:
